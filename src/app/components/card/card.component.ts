f24518e4ab5a80a763175eac1c3a9c50835de58b
M. Dogancan <dogancan.yilmazoglu@layermark.com>
2023-01-03 15:59:49 +0300

    feat: pass news info to each card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { News } from '../../interfaces/News';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  body: string = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odio facere aliquam, fuga optio illo natus ipsa doloribus eveniet explicabo unde quia, eos totam iure nisi consequuntur similique tenetur exercitationem! Placeat vero perspiciatis adipisci laborum reiciendis perferendis cum exercitationem ratione sint ut soluta tempora eaque repudiandae saepe magni laudantium repellendus quas quod, rem dolorem quaerat officiis? Vitae, illum, earum atque fuga nulla eligendi incidunt dolorem repudiandae dignissimos laboriosam magnam qui reiciendis ut? Nulla ducimus debitis commodi tempora neque expedita id magnam atque maxime enim nihil eius, aut dolore vel praesentium laudantium rem voluptates libero, quas nam. Esse et, error deleniti eum sunt sit iure asperiores sapiente alias! Quos non mollitia in aspernatur eius ab commodi eum accusamus quaerat facere fugiat beatae voluptatem ducimus, soluta labore, vitae maiores quidem accusantium! Eveniet accusantium, quibusdam ipsa animi amet recusandae atque, officia ex quod aliquam saepe dolorem eum et beatae impedit iste quae cupiditate.";
+  @Input() news: News = {id:"",title:"",imageUrl:"",body:""};
 
   constructor() { }
 
